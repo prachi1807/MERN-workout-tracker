eb8ecd932d7d5092bec6a05376ecfb6d97dc9665
prachi1807 <chodankarprachi7@gmail.com>
2023-08-30 17:33:47 +0530

    Fetch data from backend API, add proxy to handle CORS and beautify workout card

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react'
+
+// components
+import WorkoutDetails from '../components/WorkoutDetails' 
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+    
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            // array of workout objects
+            const json = await response.json()
+
+            if ( response.ok ) {
+                // update local states
+                setWorkouts(json)
+            }
+        }
+        fetchWorkouts()
+    }, []) // fire only once when component renders
+    
     return (
         <div className="home">   
-            <h2>Home</h2>
+            <div className='workouts'>
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
         </div>
     )
 }
