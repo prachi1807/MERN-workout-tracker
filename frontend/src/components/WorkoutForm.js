26a9553dc69a6659739b557c4a0b93606d741b36
prachi1807 <chodankarprachi7@gmail.com>
2023-08-31 00:23:57 +0530

    update

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -10,7 +10,7 @@ const WorkoutForm = () => {
         e.preventDefault()
 
         const workout = {title, load, reps}
-        const response = await fetch('/api/workout', {
+        const response = await fetch('/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
